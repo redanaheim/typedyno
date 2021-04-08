2f5bed2cb3f338f6c2e8ba0c0d6febc8ab78235c
TigerGold59 <Tigerball890@gmail.com>
2021-04-07 20:51:06 -0400

    Add some preliminary files
    - Logs
    - Permissions handling
    - Type utilities
    - Paste.ee

---
+++ src/typeutils.ts
@@ -0,0 +1,53 @@
+import { Snowflake } from "./permissions";
+import { Guild } from "discord.js";
+
+export const is_string = function(thing?: any): boolean {
+    if (thing === "") {
+        return true;
+    }
+    else if (!thing) {
+        return false;
+    }
+    else if (typeof thing === "string") {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+export const is_number = function(thing?: any): boolean {
+    if (!thing) {
+        return false;
+    }
+    else if (typeof thing === "number") {
+        if (isNaN(thing as number)) {
+            return false;
+        }
+        else if (!isFinite(thing as number)) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    else {
+        return false;
+    }
+}
+
+export const to_string = function(snowflake: Snowflake): string {
+    if (is_number(snowflake)) {
+        return (snowflake as number).toString();
+    }
+    else {
+        return snowflake as string;
+    }
+}
+
+export const is_server = function(guild?: Guild): boolean {
+    if (!guild) {
+        return false;
+    }
+    return true;
+}
