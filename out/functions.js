7b2005cc445e26e524adf258d28b3272b8a7a7d5
redanaheim <redanaheim@protonmail.com>
2024-11-04 20:45:23 +0100

    Delete /out

--- out/functions.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.process_message_for_commands = void 0;
-exports.process_message_for_commands = function (message, client) {
-    return false;
-};
-//# sourceMappingURL=functions.js.map
